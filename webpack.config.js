58a196dba28b8a719e3d1e23714a9f272d5719ba
qingyun_fan <qingyun_fan@rainbowtech.net.cn>
2019-12-30 17:42:48 +0800

    添加miniCssExtractPlugin使css代码从js代码中分离，添加webpack-dev-server，运行命令npx webpack-dev-server访问http://localhost:8080/src/可在浏览器调试代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+// ExtractTextPlugin不支持webpack4，替换成mini-css-extract-plugin
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
 console.log(path.resolve(__dirname, './dist'))
 console.log(__dirname)
 module.exports = {
@@ -16,16 +19,37 @@ module.exports = {
         // 用正则去匹配要用该 loader 转换的 CSS 文件
         test: /\.css$/,
         // use: ['style-loader', 'css-loader?minimize'],
+        // 提取到js
+        // use: [
+        //   'style-loader',
+        //   {
+        //     loader: 'css-loader'
+        //   }
+        // ]
+        // 单独提取到css文件
+        // use: ExtractTextPlugin.extract({
+        //   // 转换 .css 文件需要使用的 Loader
+        //   use: ['css-loader'],
+        // })
         use: [
-          'style-loader',
+          miniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
-            // options: {
-            //   minimize: true
-            // }
+            options: {
+              url: false
+            }
           }
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    // new ExtractTextPlugin({
+    //   // 从 .js 文件中提取出来的 .css 文件的名称
+    //   filename: `[name]_[contenthash:8].css`,
+    // }),
+    new miniCssExtractPlugin({
+      filename: `[name]_[contenthash:8].css`
+    })
+  ]
 };
